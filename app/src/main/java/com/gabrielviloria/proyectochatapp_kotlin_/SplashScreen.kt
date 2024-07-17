8ef194f47658777fe8ca6bc84771fff36e83a68b
vilo0812 <gabriel.viloria0812@gmail.com>
2024-07-17 15:42:50 -0500

    first commit

---
+++ app/src/main/java/com/gabrielviloria/proyectochatapp_kotlin_/SplashScreen.kt
@@ -0,0 +1,37 @@
+package com.gabrielviloria.proyectochatapp_kotlin_
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.CountDownTimer
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+           v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        MostrarBienvenida()
+    }
+    fun MostrarBienvenida(){
+        object : CountDownTimer(3000,1000){
+            override fun onTick(p0: Long) {
+                //TODO("Not yet implemented")
+            }
+
+            override fun onFinish() {
+                val intent = Intent(applicationContext, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+        }.start()
+    }
+}
